1b82ddc3a51f5722670d035a49b645c2ab1de02f
patelsa1110-design <patelsa1110@gmail.com>
2025-09-08 14:41:34 +0100

    Delete Skeltons_Time_and_Leave_PWA_Netlify_Full directory

--- Skeltons_Time_and_Leave_PWA_Netlify_Full/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('skeltons-static-v1').then(cache => cache.addAll([
-    '/', '/index.html', '/styles.css', '/app.js', '/assets/logo.png', '/manifest.webmanifest'
-  ])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
-});
